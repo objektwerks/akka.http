5dda0a40bafa65468220f117674a12169eab87cc
objektwerks <objektwerks@runbox.com>
2018-03-21 10:21:51 -0400

    add shutdown hook to now app

--- src/main/scala/akka/http/NowApp.scala
+++ src/main/scala/akka/http/NowApp.scala
@@ -5,9 +5,13 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
-class NowApp extends NowService {
+object NowApp extends NowService with App {
   implicit val system = ActorSystem.create("now", ConfigFactory.load("web/app.conf"))
   implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
   val server = Http().bindAndHandle(routes, "localhost", 7979)
+
+  sys.addShutdownHook {
+    server.flatMap(_.unbind()).onComplete(_ ⇒ system.terminate())
+  }
 }
