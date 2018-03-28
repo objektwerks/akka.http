180e4d6b2c3f5498516a8910b08b1ed32575939b
objektwerks <objektwerks@runbox.com>
2018-03-28 14:38:31 -0400

    edited app and test port

--- src/main/scala/akka/http/NowApp.scala
+++ src/main/scala/akka/http/NowApp.scala
@@ -9,7 +9,7 @@ object NowApp extends NowService with App {
   implicit val system = ActorSystem.create("now", ConfigFactory.load("app.conf"))
   implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
-  val server = Http().bindAndHandle(routes, "localhost", 7777)
+  val server = Http().bindAndHandle(routes, "localhost", 7979)
 
   sys.addShutdownHook {
     server.flatMap(_.unbind()).onComplete(_ ⇒ system.terminate())
