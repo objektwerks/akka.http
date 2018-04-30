9a301ec750e881ad37fa3ecd47bec01b1d5999b8
objektwerks <objektwerks@runbox.com>
2018-04-30 12:52:12 -0700

    refactored LowLevelHttpTest

--- src/test/scala/akka/http/LowLevelHttpTest.scala
+++ src/test/scala/akka/http/LowLevelHttpTest.scala
@@ -4,15 +4,18 @@ import java.time.LocalTime
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 import spray.json.DefaultJsonProtocol
 
-import scala.util.{Failure, Success}
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 case class Ping(now: String = LocalTime.now.toString)
 
@@ -23,8 +26,7 @@ trait PingService extends DefaultJsonProtocol with SprayJsonSupport {
 
   val requestHandler: HttpRequest => HttpResponse = {
     case HttpRequest(GET, Uri.Path("/api/v1/ping"), _, _, _) =>
-      HttpResponse(entity = HttpEntity(Ping().toJson.toString()))
-
+      HttpResponse(entity = HttpEntity(contentType = ContentTypes.`application/json`, string = Ping().toJson.toString))
     case _: HttpRequest => HttpResponse(404, entity = "Unknown resource!")
   }
 }
@@ -41,10 +43,15 @@ class LowLevelHttpTest extends FunSuite with Matchers with BeforeAndAfterAll wit
   }
 
   test("get time") {
-    val httpResponse = Http().singleRequest(HttpRequest(uri = "http://api/v1/ping"))
-    httpResponse.onComplete {
-      case Success(response) => println(response); response shouldBe Ping
-      case Failure(error) => sys.error(error.getMessage)
+    val request = RequestBuilding.Get("http://localhost:7777/api/v1/ping")
+    val future = for {
+      response <- Http().singleRequest(request)
+      entity <- Unmarshal(response.entity).to[Ping]
+    } yield {
+      println(s"get time response: $response")
+      entity
     }
+    val ping = Await.result(future, 3 seconds)
+    println(s"get time ping: $ping")
   }
 }
