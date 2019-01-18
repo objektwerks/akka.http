a1a91a5998c2e813a1171daa525399e688321cfc
objektwerks <objektwerks@gmail.com>
2019-01-18 07:40:42 -0500

    simplified RestServiceTest marshalling

--- src/test/scala/akka/http/RestServiceTest.scala
+++ src/test/scala/akka/http/RestServiceTest.scala
@@ -3,21 +3,20 @@ package akka.http
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.StatusCodes.OK
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 import spray.json.DefaultJsonProtocol
 
 trait RestService extends DefaultJsonProtocol with SprayJsonSupport {
-  import akka.http.scaladsl.marshalling._
   import akka.http.scaladsl.server.Directives._
 
   case class User(id: String, name: String)
   implicit val userFormat = jsonFormat2(User)
 
   val getByUserId = path("users" / Segment) { id =>
-    complete(ToResponseMarshallable[User](User(id, "mike")))
+    complete(OK -> User(id, "mike"))
   }
   val routes = pathPrefix("api" / "v1" / "locator") {
     getByUserId
@@ -35,7 +34,7 @@ class RestServiceTest extends WordSpec with Matchers with ScalatestRouteTest wit
   "getByUserId -> /users/{id}" should {
     "return user" in {
       Get("/api/v1/locator/users/a1") ~> routes ~> check {
-        status shouldBe StatusCodes.OK
+        status shouldBe OK
         responseAs[User] shouldEqual User("a1", "mike")
       }
     }
